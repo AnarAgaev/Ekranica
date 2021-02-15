14a8b1f44c072581571ecd4da2ed4f819482c228
Anar Agaev <anar.n.agaev@gmail.com>
2021-02-15 16:00:53 +0300

    Created grid

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Includes custom scripts
+import './js/main';
+// import './pug/templates/header/header'
+// import './pug/pages/index/index'
+
+// Include vendor styles
+import 'normalize.css';
+//import 'bootstrap/dist/css/bootstrap-grid.min.css';
+
+// Includes custom styles
+import './scss/main.scss';
