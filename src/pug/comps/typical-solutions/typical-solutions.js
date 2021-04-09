4b5b3cedb8a275043a017c61e52002e03077ba21
Anar Agaev <anar.n.agaev@gmail.com>
2021-04-09 09:58:44 +0300

    Added radion button at the button slider

--- src/pug/comps/typical-solutions/typical-solutions.js
+++ src/pug/comps/typical-solutions/typical-solutions.js
@@ -1,5 +1,13 @@
 import $ from "jquery";
 
 $(document).ready(function () {
+    const btns = $('.typical-solutions__controll-caption');
 
+    if (btns.length > 0) {
+        for (let i = 0; i < btns.length; i++) {
+            $(btns[i]).on('click', function () {
+                $(this).toggleClass('visible');
+            });
+        }
+    }
 });
