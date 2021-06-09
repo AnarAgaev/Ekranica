0f722c23465dfa26ca062fa212637e27e44c7f3c
Anar Agaev <anar.n.agaev@gmail.com>
2021-06-09 13:24:32 +0300

    Added outside screen calc

--- src/pug/comps/calc/calc.js
+++ src/pug/comps/calc/calc.js
@@ -2,5 +2,46 @@ import $ from "jquery";
 import IMask from "imask";
 
 $(document).ready(function () {
+    $(function handleClickOnAdditionallyControllersButtons() {
+        let btns = $('.calc__collapse-controllers .btn');
+
+        for ( let i = 0; i < btns.length; i++) {
+            $(btns[i]).on(
+                'click',
+                () => toggleVisibleAdditionallyCalcControllers(btns[i]),
+            );
+        }
+    });
+
+    function toggleVisibleAdditionallyCalcControllers (btn) {
+        const HIDDEN_TIMEOUT = 500; // take this param from css transition
+        let container = $(btn).parent();
+
+        if (isHidden(container)) {
+            showEl(container);
+        } else {
+            hideEl(container, HIDDEN_TIMEOUT);
+        }
+    }
+
+    function isHidden(el) {
+        return $(el).hasClass('hidden');
+    }
+
+    function showEl(el) {
+        $(el).removeClass('hidden');
+        $(el).addClass('open');
+    }
+
+    function hideEl(el, timeout) {
+        $(el).removeClass('open');
+
+        setTimeout(
+            () => {
+                $(el).addClass('hidden');
+            },
+            timeout
+        )
+    }
 
 });
