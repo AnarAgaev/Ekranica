09d5ea9346312d573a54b9915329a4d6d50d6844
Anar Agaev <anar.n.agaev@gmail.com>
2021-06-29 11:18:52 +0300

    replace cursor into input to the end of the text

--- src/pug/comps/custom-input/custom-input.js
+++ src/pug/comps/custom-input/custom-input.js
@@ -8,6 +8,10 @@ $(document).ready(function () {
             'click',
             function () {
                 let controller = $(this).children('input');
+
+                $(controller).attr('type', 'text');
+                $(controller)[0].setSelectionRange(100,100);
+                $(controller).attr('type', 'number');
                 $(controller).focus();
             }
         );
