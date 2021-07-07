8c559196a4e0c78b851cddfd35c905e8799b322d
Anar Agaev <anar.n.agaev@gmail.com>
2021-07-07 15:47:37 +0300

    Added getting year form function

--- src/js/main.js
+++ src/js/main.js
@@ -290,4 +290,20 @@ $(document).ready(function () {
     window.scrollToPageStart = function () {
         $('body, html').animate({scrollTop: 0}, 500);
     }
+
+    // Get word form for years
+    window.getYearsWordForm = function (num) {
+        let cases = [2, 0, 1, 1, 1, 2];
+        let forms = ['год', 'года', 'лет'];
+
+        let resultForm = forms[ (num%100 > 4 && num%100 < 20)
+            ? 2
+            : cases[ (num%10 < 5)
+                ? num%10
+                : 5
+                ]
+            ];
+
+        return resultForm;
+    };
 });
