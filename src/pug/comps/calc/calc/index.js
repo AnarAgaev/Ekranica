9e9ce69e886dbcb82a7b4cc811ba866c8901e32c
Anar Agaev <anar.n.agaev@gmail.com>
2021-07-08 18:37:37 +0300

    Replaced global calc values into document ready

--- src/pug/comps/calc/calc/index.js
+++ src/pug/comps/calc/calc/index.js
@@ -13,199 +13,203 @@ import './toggle-hidden-controllers';
 import './set-hidden-select-controler-value-to-state';
 import './set-checkbox-value-into-state';
 
-window.isDebugMainCalcState = false;
+$(document).ready(
+    function () {
+        window.isDebugMainCalcState = false;
 
-window.isDebugMainCalcResults = true;
+        window.isDebugMainCalcResults = true;
 
-window.CALC_PRICE = $('#mainCalc').data('calcPrice');
+        window.CALC_PRICE = $('#mainCalc').data('calcPrice');
 
-window.MAIN_CALC_STATE = {
-    calcType: 'outsideScreen', // outsideScreen, insideScreen, mediaFaced, rentScreen
-    outsideScreen: {
-        calcType: "outsideScreen",
-        location: "outdoor",
-        executionType: 'monolithic', // monolithic, cabinet
-        sizeType: [320, 160], // [320,160], [640,640], [960,960]
-        maxSizes: {
-            320160: [30400,30400],
-            640640: [30720,30720],
-            960960: [30720,30720]
-        },
-        pixelStep: undefined,
-        width: undefined,
-        height: undefined,
-        controllerParams: {
-            width: {
-                min: 320,
-                max: 30400,
-                step: 320
+        window.MAIN_CALC_STATE = {
+            calcType: 'outsideScreen', // outsideScreen, insideScreen, mediaFaced, rentScreen
+            outsideScreen: {
+                calcType: "outsideScreen",
+                location: "outdoor",
+                executionType: 'monolithic', // monolithic, cabinet
+                sizeType: [320, 160], // [320,160], [640,640], [960,960]
+                maxSizes: {
+                    320160: [30400,30400],
+                    640640: [30720,30720],
+                    960960: [30720,30720]
+                },
+                pixelStep: undefined,
+                width: undefined,
+                height: undefined,
+                controllerParams: {
+                    width: {
+                        min: 320,
+                        max: 30400,
+                        step: 320
+                    },
+                    height: {
+                        min: 160,
+                        max: 30400,
+                        step: 160
+                    }
+                },
+                QModH: undefined,
+                QModW: undefined,
+                QModSum: undefined,
+                Mod: undefined,
+                $ModSum: undefined,
+                QBp: undefined,
+                Bp: undefined,
+                $BpSum: undefined,
+                QRv: undefined,
+                Rv: undefined,
+                $RvSum: undefined,
+                $St: undefined,
+                QMag: undefined,
+                Mag: undefined,
+                $MagSum: undefined,
+                Pr: undefined,
+                $PrSum: undefined,
+                Ug: undefined,
+                $UgSum: undefined,
+                QNa: undefined,
+                Na: undefined,
+                $NaSum: undefined,
+                $Sum: undefined,
+                ExchangeRate: undefined,
+                RubSum: undefined,
+                QMk: undefined,
+                $MkSum: undefined,
+                SUType: "controller", // controller, processor
+                SU: undefined,
+                $SUSum: undefined,
+                RG: undefined, // number of years
+                $RG: undefined,
+                RS: undefined, // number of years
+                $RS: undefined,
+                DY: undefined,
+                EP: undefined,
+                ESH: undefined,
+                PM: undefined,
+                ZCH: undefined,
             },
-            height: {
-                min: 160,
-                max: 30400,
-                step: 160
-            }
-        },
-        QModH: undefined,
-        QModW: undefined,
-        QModSum: undefined,
-        Mod: undefined,
-        $ModSum: undefined,
-        QBp: undefined,
-        Bp: undefined,
-        $BpSum: undefined,
-        QRv: undefined,
-        Rv: undefined,
-        $RvSum: undefined,
-        $St: undefined,
-        QMag: undefined,
-        Mag: undefined,
-        $MagSum: undefined,
-        Pr: undefined,
-        $PrSum: undefined,
-        Ug: undefined,
-        $UgSum: undefined,
-        QNa: undefined,
-        Na: undefined,
-        $NaSum: undefined,
-        $Sum: undefined,
-        ExchangeRate: undefined,
-        RubSum: undefined,
-        QMk: undefined,
-        $MkSum: undefined,
-        SUType: "controller", // controller, processor
-        SU: undefined,
-        $SUSum: undefined,
-        RG: undefined, // number of years
-        $RG: undefined,
-        RS: undefined, // number of years
-        $RS: undefined,
-        DY: undefined,
-        EP: undefined,
-        ESH: undefined,
-        PM: undefined,
-        ZCH: undefined,
-    },
-    insideScreen: {
-        calcType: "insideScreen",
-        location: "indoor",
-        executionType: 'monolithic', // monolithic, cabinet
-        sizeType: [320, 160], // [320,160], [640,480], [640,640], [960,960]
-        maxSizes: {
-            320160: [16000,16000],
-            640480: [16000,16320],
-            640640: [16000,16000],
-            960960: [16320,16320]
-        },
-        pixelStep: undefined,
-        width: undefined,
-        height: undefined,
-        controllerParams: {
-            width: {
-                min: 320,
-                max: 16000,
-                step: 320
+            insideScreen: {
+                calcType: "insideScreen",
+                location: "indoor",
+                executionType: 'monolithic', // monolithic, cabinet
+                sizeType: [320, 160], // [320,160], [640,480], [640,640], [960,960]
+                maxSizes: {
+                    320160: [16000,16000],
+                    640480: [16000,16320],
+                    640640: [16000,16000],
+                    960960: [16320,16320]
+                },
+                pixelStep: undefined,
+                width: undefined,
+                height: undefined,
+                controllerParams: {
+                    width: {
+                        min: 320,
+                        max: 16000,
+                        step: 320
+                    },
+                    height: {
+                        min: 160,
+                        max: 16000,
+                        step: 160
+                    }
+                },
+                QModH: undefined,
+                QModW: undefined,
+                QModSum: undefined,
+                Mod: undefined,
+                $ModSum: undefined,
+                QBp: undefined,
+                Bp: undefined,
+                $BpSum: undefined,
+                QRv: undefined,
+                Rv: undefined,
+                $RvSum: undefined,
+                $St: undefined,
+                QMag: undefined,
+                Mag: undefined,
+                $MagSum: undefined,
+                Pr: undefined,
+                $PrSum: undefined,
+                Ug: undefined,
+                $UgSum: undefined,
+                QNa: undefined,
+                Na: undefined,
+                $NaSum: undefined,
+                $Sum: undefined,
+                ExchangeRate: undefined,
+                RubSum: undefined,
+                QMk: undefined,
+                $MkSum: undefined,
+                SUType: "controller", // controller, processor
+                SU: undefined,
+                $SUSum: undefined,
+                RG: undefined, // number of years
+                $RG: undefined,
+                RS: undefined, // number of years
+                $RS: undefined,
+                DY: undefined,
+                EP: undefined,
+                ESH: undefined,
+                PM: undefined,
+                ZCH: undefined,
             },
-            height: {
-                min: 160,
-                max: 16000,
-                step: 160
-            }
-        },
-        QModH: undefined,
-        QModW: undefined,
-        QModSum: undefined,
-        Mod: undefined,
-        $ModSum: undefined,
-        QBp: undefined,
-        Bp: undefined,
-        $BpSum: undefined,
-        QRv: undefined,
-        Rv: undefined,
-        $RvSum: undefined,
-        $St: undefined,
-        QMag: undefined,
-        Mag: undefined,
-        $MagSum: undefined,
-        Pr: undefined,
-        $PrSum: undefined,
-        Ug: undefined,
-        $UgSum: undefined,
-        QNa: undefined,
-        Na: undefined,
-        $NaSum: undefined,
-        $Sum: undefined,
-        ExchangeRate: undefined,
-        RubSum: undefined,
-        QMk: undefined,
-        $MkSum: undefined,
-        SUType: "controller", // controller, processor
-        SU: undefined,
-        $SUSum: undefined,
-        RG: undefined, // number of years
-        $RG: undefined,
-        RS: undefined, // number of years
-        $RS: undefined,
-        DY: undefined,
-        EP: undefined,
-        ESH: undefined,
-        PM: undefined,
-        ZCH: undefined,
-    },
-    mediaFaced: {
-        calcType: "mediaFaced",
-        executionType: 'monolithic', // monolithic, cabinet
-        sizeType: [320, 160], // [320,160], [640,480], [640,640], [960,960]
-        pixelStep: undefined,
-        width: undefined,
-        height: undefined,
-        controllerParams: {
-            width: {
-                min: undefined,
-                max: undefined,
-                step: undefined
+            mediaFaced: {
+                calcType: "mediaFaced",
+                executionType: 'monolithic', // monolithic, cabinet
+                sizeType: [320, 160], // [320,160], [640,480], [640,640], [960,960]
+                pixelStep: undefined,
+                width: undefined,
+                height: undefined,
+                controllerParams: {
+                    width: {
+                        min: undefined,
+                        max: undefined,
+                        step: undefined
+                    },
+                    height: {
+                        min: undefined,
+                        max: undefined,
+                        step: undefined
+                    }
+                }
             },
-            height: {
-                min: undefined,
-                max: undefined,
-                step: undefined
-            }
+            rentScreen: {
+                calcType: "rentScreen",
+                rentConstruction: 'monolithic', // outdoor, Подвесная
+                sizeType: [320, 160], // [320,160], [640,480], [640,640], [960,960]
+                pixelStep: undefined,
+                width: undefined,
+                height: undefined,
+                controllerParams: {
+                    width: {
+                        min: undefined,
+                        max: undefined,
+                        step: undefined
+                    },
+                    height: {
+                        min: undefined,
+                        max: undefined,
+                        step: undefined
+                    }
+                }
+            },
+        };
+
+        window.executionTypeTimer = {
+            pixelStep: undefined,
+            executionType: undefined
+        };
+
+        window.printMainState = function() {
+            console.log(
+                'Current STATE of all calculators: ',
+                MAIN_CALC_STATE
+            );
+        };
+
+        window.getActiveMainCalc = function() {
+            return $('.calc__calculator.active')[0];
         }
-    },
-    rentScreen: {
-        calcType: "rentScreen",
-        rentConstruction: 'monolithic', // outdoor, Подвесная
-        sizeType: [320, 160], // [320,160], [640,480], [640,640], [960,960]
-        pixelStep: undefined,
-        width: undefined,
-        height: undefined,
-        controllerParams: {
-            width: {
-                min: undefined,
-                max: undefined,
-                step: undefined
-            },
-            height: {
-                min: undefined,
-                max: undefined,
-                step: undefined
-            }
-        }
-    },
-};
-
-window.executionTypeTimer = {
-    pixelStep: undefined,
-    executionType: undefined
-};
-
-window.printMainState = function() {
-    console.log(
-        'Current STATE of all calculators: ',
-        MAIN_CALC_STATE
-    );
-};
-
-window.getActiveMainCalc = function() {
-    return $('.calc__calculator.active')[0];
-}
+    }
+);
